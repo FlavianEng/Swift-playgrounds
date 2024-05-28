31b89cd76e0c52a19ce8c74ebb4c786c33ac9065
Flavian <engevinflavian@gmail.com>
2024-05-28 16:16:48 +0200

    feat(ChutesAndLadders): generate ladders and snakes values and positions

--- ChutesAndLadders.playground/Contents.swift
+++ ChutesAndLadders.playground/Contents.swift
@@ -3,21 +3,21 @@
  
  The rules of the game are as follows:
  
- - The board has 25 squares, and the aim is to land on or beyond square 25.
- - The player’s starting square is “square zero”, which is just off the bottom-left corner of the board.
- - Each turn, you roll a six-sided dice and move by that number of squares, following the horizontal path indicated by the dotted arrow above.
- - The game contains 4 ladders and 4 snakes
- - If your turn ends at the bottom of a ladder, you move up that ladder.
- - If your turn ends at the head of a snake, you move down that snake.
+ - [x] The board has 25 squares, and the aim is to land on or beyond square 25.
+ - [x] The player’s starting square is “square zero”, which is just off the bottom-left corner of the board.
+ - [x] Each turn, you roll a six-sided dice and move by that number of squares, following the horizontal path indicated by the dotted arrow above.
+ - [x] The game contains 4 ladders and 4 snakes
+ - [x] If your turn ends at the bottom of a ladder, you move up that ladder.
+ - [x] If your turn ends at the head of a snake, you move down that snake.
  
  * Bonus *
- - Ladders and snakes numbers are dynamic with squares count. Cannot be below 1 each.
- - Ladders and snakes positions are randomized.
- - The aim is to land precisely on final square.
+ - [x] Ladders and snakes numbers are dynamic with squares count. Cannot be below 1 each.
+ - [x] Ladders and snakes positions are randomized.
+ - [ ] The aim is to land precisely on final square.
  
  * Bonus x2 *
- - Change final square value
- - Calculates and indicates best score
+ - [ ] Change final square value
+ - [ ] Calculates and indicates best score
  
  Answer: https://docs.swift.org/swift-book/documentation/the-swift-programming-language/controlflow
  */
@@ -26,7 +26,7 @@ let finalSquare = 25
 var board: [Int] = Array(repeating: 0, count: finalSquare + 1)
 
 func start() -> Void {
-    initGame()
+    initGame(finalSquare: finalSquare)
     
     var score = 0
     var currentSquare: Int = 0
@@ -45,24 +45,50 @@ func start() -> Void {
     print("You've done well with a score of \(score)")
 }
 
-func initGame() -> Void {
-    setLadders()
-    setSnakes()
+func initGame(finalSquare: Int) -> Void {
+    setLadders(finalSquare: finalSquare)
+    setSnakes(finalSquare: finalSquare)
 }
 
-func setLadders() -> Void {
-    board[03] = 11
-    board[06] = 17
-    board[09] = 18
-    board[10] = 12
+func setLadders(finalSquare: Int) -> Void {
+    let laddersCount = Int(finalSquare / 6)
+    let maxLadderLength = Int(Double(finalSquare) * (30/100))
+    let maxLadderPosition = Int(Double(finalSquare) * (70/100)) - maxLadderLength
+    
+    var ladderPositions: Set<Int> = []
+    
+    while ladderPositions.count < laddersCount {
+        ladderPositions.insert(Int.random(in: 1...maxLadderPosition))
+    }
+    
+    for ladderPosition in ladderPositions {
+        board[ladderPosition] = ladderPosition + Int.random(in: 1...maxLadderLength)
+    }
 }
 
-func setSnakes() -> Void {
-    board[14] = 4
-    board[19] = 8
-    board[22] = 20
-    board[24] = 16
+func setSnakes(finalSquare: Int) -> Void {
+    let snakesCount = Int(finalSquare / 6)
+    let maxSnakeLength = Int(Double(finalSquare) * (40/100))
+    let minSnakePosition = Int(Double(finalSquare) * (10/100)) + maxSnakeLength
+    
+    var snakePositions: Set<Int> = []
+    
+    while snakePositions.count < snakesCount {
+        snakePositions.insert(Int.random(in: minSnakePosition...(finalSquare-1)))
+    }
+    
+    var snakePlaced = 0
+    while snakePlaced < snakesCount {
+        let snakeHeadPosition = Int.random(in: minSnakePosition...finalSquare-1)
+        let snakeTailPosition = snakeHeadPosition - Int.random(in: 1...maxSnakeLength)
+        
+        if (board[snakeHeadPosition] != 0) {
+            continue
+        }
+        
+        board[snakeHeadPosition] = snakeTailPosition
+        snakePlaced += 1
+    }
 }
 
 start()
-
